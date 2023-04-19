2e563e4b9df4b2395384fc11f08b319c29b0dbe8
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2023-04-19 22:03:05 +0300

    added a function to add new tasks

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Platform, View, Text } from "react-native";
+import {
+  StyleSheet,
+  Platform,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  text,
+} from "react-native";
 import {
   Button,
   Card,
@@ -9,6 +17,7 @@ import {
   ThemeProvider,
   darkColors,
   Icon,
+  FAB,
 } from "@rneui/themed";
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 const theme = createTheme({
@@ -75,12 +84,42 @@ const App = () => {
   // }, [])
 
   const [searchValue, setSearchValue] = useState("");
+  const [isHidden, setIsHidden] = useState(true);
+  const [newTitle, setnewTitle] = useState("");
+  const [newDescription, setnewDescription] = useState("");
+  const [newDate, setnewDate] = useState("");
   const filterNotes = () => {
     if (searchValue === "") return;
     const filteredNotes = notes.filter((note) =>
       note.title.toLowerCase().includes(searchValue.toLowerCase())
     );
     setFilteredNotes(filteredNotes);
+  };
+  const addNotes = (title, desc, deadline) => {
+    //console.log("addclick");
+    setIsHidden(!isHidden);
+    //console.log(notes);
+    notesCopy = [...notes];
+    newNotesArray = notesCopy.concat([
+      {
+        id: notes[notes.length - 1].id + 1,
+        content: desc,
+        title: title,
+        dateCreated: new Date(),
+        deadline: deadline,
+      },
+    ]);
+    // console.log(newNotesArray);
+    setNotes(newNotesArray);
+    //console.log(notes);
+    setFilteredNotes(newNotesArray);
+  };
+  const handleAdd = () => {
+    //console.log("handleadd");
+    //console.log(newTitle);
+    //console.log(newDescription);
+    // console.log(newDate);
+    addNotes(newTitle, newDescription, newDate);
   };
   return (
     <ThemeProvider theme={theme}>
@@ -90,7 +129,7 @@ const App = () => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-
+          //overflow: "scrollY",
           marginTop: 10,
         }}
       >
@@ -125,55 +164,152 @@ const App = () => {
           />
           <Icon name="menu" size={30} style={{ padding: 20 }} />
         </View>
-        {filteredNotes.map((note) => (
-          <Card key={note.id} containerStyle={{ width: "100%", margin: 10 }}>
-            <View
-              style={{ flexDirection: "row", justifyContent: "space-between" }}
-            >
-              <Card.Title>{note.title}</Card.Title>
-            </View>
-            <Card.Divider />
-            <View
-              style={{
-                width: "100%",
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
+        <ScrollView
+          style={{
+            height: "80%",
+            width: "100%",
+          }}
+          contentContainerStyle={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {filteredNotes.map((note) => (
+            <Card key={note.id} containerStyle={{ width: "90%", margin: 10 }}>
               <View
                 style={{
-                  width: "70%",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
                 }}
               >
-                <Text>{note.content}</Text>
+                <Card.Title>{note.title}</Card.Title>
               </View>
+              <Card.Divider />
               <View
                 style={{
-                  width: "20%",
+                  width: "100%",
                   flexDirection: "row",
                   alignItems: "center",
                 }}
               >
-                <Icon
-                  name="pencil"
-                  type="font-awesome"
-                  size={15}
-                  reverse
-                  onPress={() => console.log("Edit button pressed")}
-                />
-                <Icon
-                  name="trash"
-                  type="font-awesome"
-                  size={15}
-                  reverse
-                  onPress={() => console.log("Delete button pressed")}
-                />
+                <View
+                  style={{
+                    width: "70%",
+                  }}
+                >
+                  <Text>{note.content}</Text>
+                </View>
+                <View
+                  style={{
+                    width: "20%",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <Icon
+                    name="pencil"
+                    type="font-awesome"
+                    size={15}
+                    reverse
+                    onPress={() => console.log("Edit button pressed")}
+                  />
+                  <Icon
+                    name="trash"
+                    type="font-awesome"
+                    size={15}
+                    reverse
+                    onPress={() => console.log("Delete button pressed")}
+                  />
+                </View>
               </View>
-            </View>
-          </Card>
-        ))}
+            </Card>
+          ))}
+        </ScrollView>
       </View>
+      {!isHidden && (
+        <View
+          style={{
+            width: "80%",
+            display: "flex",
+            height: 400,
+            justifyContent: "center",
+            alignItems: "center",
+            //overflow: "scrollY",
+            marginTop: 10,
+            position: "absolute",
+            backgroundColor: "#fff",
+            borderWidth: 5,
+            right: "10%",
+            bottom: 120,
+          }}
+        >
+          <Text>Title</Text>
+          <TextInput
+            style={{
+              height: 40,
+              margin: 12,
+              borderWidth: 1,
+              padding: 10,
+              width: 300,
+            }}
+            onChangeText={(text) => setnewTitle(text)}
+          />
+          <Text>description</Text>
+          <TextInput
+            style={{
+              height: 40,
+              margin: 12,
+              borderWidth: 1,
+              padding: 10,
+              width: 300,
+            }}
+            onChangeText={(text) => setnewDescription(text)}
+          />
+          <Text>Due by date</Text>
+          <TextInput
+            style={{
+              height: 40,
+              margin: 12,
+              borderWidth: 1,
+              padding: 10,
+              width: 300,
+            }}
+            onChangeText={(text) => setnewDate(text)}
+          />
+          <Text>test</Text>
+          <Button title="add button" onPress={() => handleAdd()} />
+        </View>
+      )}
+      <FAB
+        icon={{ name: "add", color: "white" }}
+        color="green"
+        style={{
+          position: "absolute",
+          width: 50,
+          height: 50,
+          alignItems: "center",
+          justifyContent: "center",
+          right: 170,
+          bottom: 50,
+        }}
+        onPress={() => setIsHidden(!isHidden)}
+      />
     </ThemeProvider>
   );
 };
+const styles = StyleSheet.create({
+  addButton: {
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 70,
+    position: "absolute",
+    bottom: 0,
+    right: 10,
+    height: 70,
+    backgroundColor: "#fff",
+    borderRadius: 100,
+  },
+});
 export default App;
